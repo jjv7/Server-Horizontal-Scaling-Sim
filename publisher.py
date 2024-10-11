d0bf598e202175505cc513fbfc04e9bdba30c980
jjv7 <jaydenzikong@gmail.com>
2024-10-11 16:43:08 +1100

    Add files

---
+++ publisher.py
@@ -0,0 +1,63 @@
+from paho.mqtt import client as mqtt_client
+from dotenv import load_dotenv
+import os
+import random
+import time
+
+load_dotenv()
+
+# This code uses the Paho MQTT Python Client library
+# References: https://www.emqx.com/en/blog/how-to-use-mqtt-in-python
+#             https://github.com/eclipse/paho.mqtt.python/blob/master/docs/migrations.rst
+
+# Connection info
+broker = os.getenv('BROKER')
+port = 1883
+topic = "<104547242>/temperature"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = os.getenv('MQTT_USERNAME')
+password = os.getenv('MQTT_PASSWORD')
+
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc, properties):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client):
+    msg_count = 1
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+        if msg_count > 5:
+            break
+
+
+def main():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+    client.loop_stop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ subscriber.py
@@ -0,0 +1,50 @@
+from paho.mqtt import client as mqtt_client
+import os
+import random
+
+
+# This code uses the Paho MQTT Python Client library
+# References: https://www.emqx.com/en/blog/how-to-use-mqtt-in-python
+#             https://github.com/eclipse/paho.mqtt.python/blob/master/docs/migrations.rst
+
+
+# Connection info
+broker = os.getenv('BROKER')
+port = 1883
+topic = "<104547242>/temperature"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = os.getenv('MQTT_USERNAME')
+password = os.getenv('MQTT_PASSWORD')
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc, properties):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def main():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    main()
