174e735a67e1ca99cfb5a284efd743b34046fb9d
jjv7 <jaydenzikong@gmail.com>
2024-10-13 23:39:19 +1100

    Start of tkinter program

---
+++ gui_mqtt_client.py
@@ -0,0 +1,11 @@
+from paho.mqtt import client as mqtt_client
+import tkinter as tk
+from tkinter import ttk, messagebox
+import random
+
+def main():
+    window = tk.Tk()
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
