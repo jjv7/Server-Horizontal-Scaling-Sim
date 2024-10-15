bcc0927a6bc45d54edc28510f1324d6b7c83fb08
jjv7 <jaydenzikong@gmail.com>
2024-10-15 18:16:28 +1100

    Finished UI for messages tab

--- gui_mqtt_client.py
+++ gui_mqtt_client.py
@@ -22,7 +22,7 @@ class MqttClientGui(tk.Tk):
         super().__init__()
         
         # Initialise window
-        self.geometry("800x450")              # 16:9 aspect ratio
+        self.geometry("585x450")              # 16:9 aspect ratio
         self.title("MQTT Python GUI Client")
         self.resizable(False, False)          # Keep window size fixed, so I don't have to do reactive windows
 
@@ -65,9 +65,9 @@ class MqttClientGui(tk.Tk):
         hostFrame.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
         
         hostLabel = ttk.Label(hostFrame, text="mqtt://")
-        hostLabel.grid(row=0, column=0, pady=(0, 7))
+        hostLabel.grid(row=0, column=0, padx=(2, 3), pady=(0, 7))
         
-        self.hostEntry = ttk.Entry(hostFrame, width=23)                               # Entries will require self, since we need to call them in another function later
+        self.hostEntry = ttk.Entry(hostFrame, width=22)                               # Entries will require self, since we need to call them in another function later
         self.hostEntry.grid(row=0, column=1, pady=(0, 7), sticky=tk.W)
 
 
@@ -106,6 +106,48 @@ class MqttClientGui(tk.Tk):
     def initMessageTab(self, messageTab):
         tabTitle = ttk.Label(messageTab, text="Messages", font="Calibri, 18 bold")
         tabTitle.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
+
+
+        # Create subscribe section
+        subFrame = ttk.LabelFrame(messageTab, text="Subscribe", padding=(10,10))
+        subFrame.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+        subTopicsLabel = ttk.Label(subFrame, text="Topics:")
+        subTopicsLabel.grid(row=0, column=0, sticky=tk.W)
+
+        subTopicsEntry = ttk.Entry(subFrame)
+        subTopicsEntry.grid(row=0, column=1, padx=(22, 0), pady=10, sticky=tk.W)
+
+        subButton = ttk.Button(subFrame, text="Subscribe")
+        subButton.grid(row=1, column=0, columnspan=2, pady=10)
+        
+
+        # Create publish section
+        pubFrame = ttk.LabelFrame(messageTab, text="Publish", padding=(10,10))
+        pubFrame.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W + tk.N)
+
+        pubTopicsLabel = ttk.Label(pubFrame, text="Topics:")
+        pubTopicsLabel.grid(row=0, column=0, sticky=tk.W + tk.N)
+
+        pubTopicsEntry = ttk.Entry(pubFrame)
+        pubTopicsEntry.grid(row=0, column=1, padx=(10, 0), pady=(0, 10), sticky=tk.W)
+
+        pubMessageLabel = ttk.Label(pubFrame, text="Message:")
+        pubMessageLabel.grid(row=1, column=0, sticky=tk.W)
+
+        pubMessageEntry = ttk.Entry(pubFrame)
+        pubMessageEntry.grid(row=1, column=1, padx=(10, 0), pady=10, sticky=tk.W)
+
+        pubButton = ttk.Button(pubFrame, text="Publish")
+        pubButton.grid(row=2, column=0, columnspan=2, pady=10)
+
+        # Create messages section
+        messagesFrame = ttk.LabelFrame(messageTab, text="Received Messages", padding=(10,10))
+        messagesFrame.grid(row=1, column=1, rowspan=2, padx=10, pady=10, sticky=tk.W)
+
+        self.messagesDisplay = tk.Listbox(messagesFrame, height=17, width=50)
+        self.messagesDisplay.grid(row=0, column=0)
+
 
 
 
